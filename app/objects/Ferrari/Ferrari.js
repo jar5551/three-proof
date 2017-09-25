92060ca26dbe756f13da8175f46a341d61153507
Jarosław Frydrych <jarek.frydrych@gmail.com>
2017-09-25 23:42:48 +0200

    model

---
+++ app/objects/Ferrari/Ferrari.js
@@ -0,0 +1,27 @@
+import { Group, Mesh, MeshStandardMaterial, PlaneGeometry, RepeatWrapping } from 'three';
+import MODEL from './scene.json';
+import { loadModel, loadScene, loadTextureSet, GetAsset } from '../../Loaders/Loader';
+
+export default class extends Group {
+  constructor() {
+    super();
+
+    this.loadingFunction = (p) => {
+      console.log('loading', p)
+    }
+    this.name = 'bunny';
+    this.load();
+  }
+
+  async load() {
+    console.log('Loading bunny scene');
+    const model = await loadScene(MODEL, this.loadingFunction);
+    console.log('Loading bunny geo');
+    console.log('Done loading');
+
+
+    //model.position.set(1.2 / 2, -0.3, 0);
+
+    this.add(model);
+  }
+}
